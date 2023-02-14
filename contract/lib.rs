55b6382c7dc43f141869e197d9ee39032b041b69
pcorrado-np <pcorrado@neopower.digital>
2023-02-14 15:56:32 -0300

    feat: Add members, max_points_per_sender & admin
    - Members management: Add & Remove
    - Contract creator -> Admin
    - Maximum points per sender configuration by message

--- contract/lib.rs
+++ contract/lib.rs
@@ -5,6 +5,24 @@ mod syndeo {
     use ink::storage::Mapping;
     use ink_prelude::vec::Vec;
 
+    #[derive(PartialEq, Debug, Eq, Clone, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum ContractError {
+        MemberAlreadyExists,
+        MemberDoesNotExist,
+        MaxPointsPerSenderCannotBeZero,
+    }
+
+    #[ink(event)]
+    pub struct NewMember {
+        member: AccountId,
+    }
+
+    #[ink(event)]
+    pub struct MemberDeleted {
+        member: AccountId,
+    }
+
     #[ink(event)]
     pub struct Contribution {
         sender: AccountId,
@@ -19,21 +37,74 @@ mod syndeo {
 
     #[ink(storage)]
     pub struct Syndeo {
+        admin: AccountId,
+        members: Vec<AccountId>,
+        points_by_sender: Mapping<AccountId, u64>,
         points_by_recipient: Mapping<AccountId, u64>,
         recipients: Vec<AccountId>,
         total_points: u64,
+        max_points_per_sender: u64,
     }
 
     impl Syndeo {
         #[ink(constructor)]
-        pub fn new() -> Self {
+        pub fn new(max_points_per_sender: Option<u64>) -> Self {
             Self {
+                admin: Self::env().caller(),
+                members: Vec::new(),
+                points_by_sender: Mapping::default(),
                 points_by_recipient: Mapping::default(),
                 recipients: Vec::new(),
                 total_points: 0,
+                max_points_per_sender: max_points_per_sender.unwrap_or(10),
             }
         }
 
+        // ONLY ADMIN
+        #[ink(message)]
+        pub fn add_member(&mut self, new_member: AccountId) -> Result<(), ContractError> {
+            if self.members.contains(&new_member) {
+                return Err(ContractError::MemberAlreadyExists);
+            }
+
+            self.members.push(new_member);
+            self.env().emit_event(NewMember { member: new_member });
+
+            Ok(())
+        }
+
+        // ONLY ADMIN
+        #[ink(message)]
+        pub fn remove_member(&mut self, member_to_remove: AccountId) -> Result<(), ContractError> {
+            match self.members.iter().position(|m| *m == member_to_remove) {
+                Some(member_index) => {
+                    self.members.remove(member_index);
+                    self.env().emit_event(MemberDeleted {
+                        member: member_to_remove,
+                    });
+                }
+                None => return Err(ContractError::MemberDoesNotExist),
+            };
+
+            Ok(())
+        }
+
+        // ONLY ADMIN
+        #[ink(message)]
+        pub fn set_max_points_per_sender(
+            &mut self,
+            max_points_per_sender: u64,
+        ) -> Result<(), ContractError> {
+            if self.max_points_per_sender == 0 {
+                return Err(ContractError::MaxPointsPerSenderCannotBeZero);
+            }
+
+            self.max_points_per_sender = max_points_per_sender;
+
+            Ok(())
+        }
+
+        // ONLY MEMBERS (Sender & Recipient)
         #[ink(message)]
         pub fn award(&mut self, recipient: AccountId, amount: u64) {
             let sender = self.env().caller();
@@ -57,6 +128,7 @@ mod syndeo {
             }
         }
 
+        // ONLY ADMIN
         #[ink(message)]
         pub fn distribute_rewards(&mut self) {
             let total_reward: Balance = self.env().balance();
